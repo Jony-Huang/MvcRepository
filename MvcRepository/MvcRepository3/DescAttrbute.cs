77447d3dad108470b1ec53c4c5c324a335e70bc9
Jony-Huang <hjfbbs@163.com>
2017-01-15 00:43:51 +0800

    添加特性类

---
+++ MvcRepository/MvcRepository3/DescAttrbute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcRepository3
+{
+    public class DescAttrbute : Attribute
+    {
+        public string Name { get; set; }
+    }
+}
