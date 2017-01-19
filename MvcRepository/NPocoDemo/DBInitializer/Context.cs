f90b714b32b80d596dfe78308f539c356bd397d6
Jony-Huang <hjfbbs@163.com>
2017-01-19 14:35:06 +0800

    用例

---
+++ MvcRepository/NPocoDemo/DBInitializer/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NPoco;
+using NPocoDemo.Models;
+
+namespace NPocoDemo.DBInitializer
+{
+    public class Context
+    {
+        //public Context() : base("") { }
+
+        //public DbSet<User> User { get; set; }
+        //public DbSet<Role> Role { get; set; }
+        //public DbSet<Menu> Menu { get; set; }
+        //public DbSet<UserRole> UserRole { get; set; }
+        //public DbSet<RoleMenu> RoleMenu { get; set; }
+
+    }
+}
